d02ce65fc9e2b8c9e9939c8484a04c3fc1f42ab5
Thyago Santorini <admin@thyagosantorini.com>
2020-10-19 00:49:54 -0300

    update index and script

--- snake-game-javascript/script.js
+++ snake-game-javascript/script.js
@@ -3,8 +3,8 @@ let context = canvas.getContext("2d");
 let box = 32;
 
 function criarBG() {
-    context.filStyle = "lightgreen";
-    context.filRect(0, 0, 16 * box, 16 * box);
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 criarBG();
