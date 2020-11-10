66464030f8564d8cd809a59e98a208de5e072d18
Thyago Santorini <thyago@thyagosantorini.com>
2020-11-09 22:04:07 -0300

    Update aula 08

--- helicopter-shooter-game/js/js.js
+++ helicopter-shooter-game/js/js.js
@@ -12,6 +12,24 @@ function start() { // Inicio da função start()
 //Principais variáveis do jogo
 	
 var jogo = {};
+var TECLA = {
+W: 87,
+S: 83,
+D: 68
+}
+
+jogo.pressionou = [];
+
+//Verifica se o usuário pressionou alguma tecla	
+	
+$(document).keydown(function(e){
+	jogo.pressionou[e.which] = true;
+	});
+
+
+	$(document).keyup(function(e){
+    jogo.pressionou[e.which] = false;
+	});
 	
 //Game Loop
 
@@ -20,6 +38,7 @@ jogo.timer = setInterval(loop,30);
 function loop() {
 
 movefundo();
+movejogador();
 
 } // Fim da função loop()
 
@@ -33,3 +52,24 @@ esquerda = parseInt($("#fundoGame").css("background-position"));
 $("#fundoGame").css("background-position",esquerda-1);
 
 } // fim da função movefundo()
+
+function movejogador() {
+	
+	if (jogo.pressionou[TECLA.W]) {
+		var topo = parseInt($("#jogador").css("top"));
+		$("#jogador").css("top",topo-10);
+	
+	}
+	
+	if (jogo.pressionou[TECLA.S]) {
+		
+		var topo = parseInt($("#jogador").css("top"));
+		$("#jogador").css("top",topo+10);	
+	}
+	
+	if (jogo.pressionou[TECLA.D]) {
+		
+		//Chama função Disparo	
+	}
+
+	} // fim da função movejogador()
